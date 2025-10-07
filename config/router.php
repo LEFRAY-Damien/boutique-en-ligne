9146b995732aa3f703d7b63dfa40635f7c38e395
Damien LEFRAY <damienlefray@gmail.com>
2025-10-07 10:47:36 +0200

    routeur ok

---
+++ config/router.php
@@ -0,0 +1,24 @@
+<?php
+
+class Router{
+
+    public static function redirect()
+    {
+        if (isset($_GET['page'])) {
+            switch ($_GET['page']) {
+
+                case 'login':
+                    $controller = new UserController();
+                    $controller->login();
+                    break;
+                case 'liste-user':
+                    $controller = new UserController();
+                    $controller->liste();
+                    break;
+                default:
+                    echo 'Page not found';
+                    break;
+            }
+        }
+    }
+}
