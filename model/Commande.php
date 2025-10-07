e14b5be323513d8de912127b659997691b60e48c
Damien LEFRAY <damienlefray@gmail.com>
2025-10-07 11:31:45 +0200

    poo utilisateur & commande

---
+++ model/Commande.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+class Commande
+{
+
+    // Des propriétés (id, nom, email)
+    private int $id;
+    private string $date;
+    private string $status;
+
+        /**
+     * Get the value of email
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setLogin($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+        /**
+     * Get the value of email
+     */ 
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+}
+
+?>
---
+++ model/Utilisateur.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+class User
+{
+
+    // Des propriétés (id, nom, email)
+    private int $id;
+    private string $login;
+    private string $mdp;
+    private string $role;  
+  
+       /**
+     * Get the value of email
+     */ 
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+
+           /**
+     * Get the value of email
+     */ 
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setMdp($mdp)
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+           /**
+     * Get the value of email
+     */ 
+    public function getrole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+           /**
+     * Get the value of email
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+   
+
+}
+
+?>
